7a2e5e768470977b82aea1efea22da0b3fe1fd83
Amir <baradaran13085@gmail.com>
2025-02-21 00:27:49 +0330

    Update index.jsx

--- Food-Web/src/Pages/Home/index.jsx
+++ Food-Web/src/Pages/Home/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import SliderHm from './SliderHm'
+import CategoriesHm from './CategoriesHm'
+import Offers from './Offers'
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <>
+     <SliderHm/> 
+     <CategoriesHm/>
+     <Offers/>
+    </>
   )
 }

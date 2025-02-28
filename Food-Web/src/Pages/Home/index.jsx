b65b841db0e3cd6b40ab91412e35130901373177
Amir <baradaran13085@gmail.com>
2025-02-28 23:19:39 +0330

    Update index.jsx

--- Food-Web/src/Pages/Home/index.jsx
+++ Food-Web/src/Pages/Home/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import FirstPart from "./FirstPart";
 import AboutUsHm from "./AboutUsHm";
+import CategoriesHm from "./CategoriesHm";
 export default function Home() {
   return (
     <>
       <FirstPart />
       <AboutUsHm/>
+      <CategoriesHm/>
+      
     </>
   );
 }

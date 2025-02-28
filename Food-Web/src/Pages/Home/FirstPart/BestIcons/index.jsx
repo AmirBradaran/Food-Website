dfbcef425889e9196f01881b0d5b62e68999c0fa
Amir <baradaran13085@gmail.com>
2025-02-28 16:00:42 +0330

    Update index.jsx

--- Food-Web/src/Pages/Home/FirstPart/BestIcons/index.jsx
+++ Food-Web/src/Pages/Home/FirstPart/BestIcons/index.jsx
@@ -1,9 +1,78 @@
-import React from 'react'
-
+import { Box, Stack, Typography } from "@mui/material";
+import React from "react";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
 export default function BestIcons() {
   return (
-    <div>
-      
-    </div>
-  )
+    <Stack
+      direction={"row"}
+      gap={5}
+      position={"absolute"}
+      top={"65%"}
+      sx={{ right: { mx: "1%", sm: "5%" } }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          backgroundColor: "#222",
+          color: "white",
+          padding: "10px 15px",
+          borderRadius: "5px 20px 5px 20px",
+          width: "200px",
+          justifyContent: "space-between",
+          boxShadow: 3,
+        }}
+      >
+        <Typography sx={{ fontSize: "25px", fontWeight: "bold" }}>
+          کیفیت بالا
+        </Typography>
+        <Box
+          sx={{
+            backgroundColor: "white",
+            borderRadius: "10px",
+            padding: "5px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            width: "45px",
+            height: "45px",
+          }}
+        >
+          <InsertEmoticonIcon sx={{ color: "black", fontSize: "35px" }} />
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          backgroundColor: "#222",
+          color: "white",
+          padding: "10px 15px",
+          borderRadius: "20px 5px 20px 5px",
+          width: "200px",
+          justifyContent: "space-between",
+          boxShadow: 3,
+        }}
+      >
+        <Typography sx={{ fontSize: "25px", fontWeight: "bold" }}>
+          دریافت سریع
+        </Typography>
+        <Box
+          sx={{
+            backgroundColor: "white",
+            borderRadius: "10px",
+            padding: "5px",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            width: "45px",
+            height: "45px",
+          }}
+        >
+          <LocalShippingIcon sx={{ color: "black" }} />
+        </Box>
+      </Box>
+    </Stack>
+  );
 }

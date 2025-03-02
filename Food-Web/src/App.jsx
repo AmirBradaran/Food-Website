9c63e7308183a38ae6628e4fdb7e8962b43e0333
Amir <baradaran13085@gmail.com>
2025-03-02 13:39:05 +0330

    Update App.jsx

--- Food-Web/src/App.jsx
+++ Food-Web/src/App.jsx
@@ -1,35 +1,42 @@
-import { Box, CssBaseline } from '@mui/material';
-import React from 'react'
-import { useSelector } from 'react-redux';
-import Navbar from "./Components/Navbar"
-import { Navigate, Route, Routes } from 'react-router-dom';
-import Home from "./Pages/Home"
-import Auth from "./Pages/Auth"
-import AboutUs from "./Pages/AboutUs"
-import Menu from "./Pages/Menu"
-import NotFound from "./Pages/NotFound"
-import Profile from "./Pages/Profile"
-import CartBox from "./Pages/CartBox"
-import Footer from './Components/Footer';
+import { Box, CssBaseline } from "@mui/material";
+import React from "react";
+import { useSelector } from "react-redux";
+import Navbar from "./Components/Navbar";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Auth from "./Pages/Auth";
+import AboutUs from "./Pages/AboutUs";
+import Menu from "./Pages/Menu";
+import NotFound from "./Pages/NotFound";
+import Profile from "./Pages/Profile";
+import CartBox from "./Pages/CartBox";
+import Footer from "./Components/Footer";
+import Orders from "./Pages/Orders";
 export default function App() {
   const { token } = useSelector((state) => state.auth);
   return (
-<Box>
-      <CssBaseline/>
-      <Navbar/>
+    <Box>
+      <CssBaseline />
+      <Navbar />
       <Box>
         <Routes>
-          <Route exact path='/' element={< Home />} />
-          <Route path='/auth' element={< Auth />} />
-          <Route path='/menu' element={< Menu />} />
-          <Route path='/about-us' element={< AboutUs />} />
-          <Route path='*' element={< NotFound />} />
-          {/* <Route path='/' element={< Order />} /> */}
-          <Route path='/profile' element={token ? < Profile /> : <Navigate to={"/auth"} />} />
-          <Route path='/cart-box' element={ token ? < CartBox /> : <Navigate to={"/auth"}/>} />
+          <Route exact path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/about-us" element={<AboutUs />} />
+          <Route path="*" element={<NotFound />} />
+          <Route path="/orders" element={< Orders />} />
+          <Route
+            path="/profile"
+            element={token ? <Profile /> : <Navigate to={"/auth"} />}
+          />
+          <Route
+            path="/cart-box"
+            element={token ? <CartBox /> : <Navigate to={"/auth"} />}
+          />
         </Routes>
       </Box>
-      <Footer/>
+      <Footer />
     </Box>
-  )
+  );
 }

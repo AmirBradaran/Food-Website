1a1ac050e2f4ee20ef73da236d1d4a82fde36842
Amir <baradaran13085@gmail.com>
2025-03-02 13:39:15 +0330

    Update index.jsx

--- Food-Web/src/Pages/Home/FirstPart/BestIcons/index.jsx
+++ Food-Web/src/Pages/Home/FirstPart/BestIcons/index.jsx
@@ -2,15 +2,19 @@ import { Box, Stack, Typography } from "@mui/material";
 import React from "react";
 import LocalShippingIcon from "@mui/icons-material/LocalShipping";
 import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
+
 export default function BestIcons() {
   return (
     <Stack
-      direction={"row"}
-      gap={5}
-      position={"absolute"}
-      top={"65%"}
-      sx={{ right: { mx: "1%", sm: "5%" } }}
+      direction="row"
+      gap={3}
+      position="absolute"
+      top="65%"
+      sx={{
+        right: { xs: "0.5%", sm: "5%" },
+      }}
     >
+      {/* Box 1 */}
       <Box
         sx={{
           display: "flex",
@@ -18,14 +22,15 @@ export default function BestIcons() {
           backgroundColor: "#222",
           color: "white",
           padding: "10px 15px",
-          paddingLeft:"30px",
+          paddingLeft: "30px",
           borderRadius: "5px 20px 5px 20px",
-          width: "200px",
+          width: "auto",
+          minWidth: "180px",
           justifyContent: "space-between",
           boxShadow: 3,
         }}
       >
-        <Typography sx={{ fontSize: "25px", fontWeight: "bold" }}>
+        <Typography sx={{ fontSize: "20px", fontWeight: "bold" , fontFamily:"IranYekan" }}>
           کیفیت بالا
         </Typography>
         <Box
@@ -40,9 +45,11 @@ export default function BestIcons() {
             height: "45px",
           }}
         >
-          <InsertEmoticonIcon sx={{ color: "black", fontSize: "35px" }} />
+          <InsertEmoticonIcon sx={{ color: "black", fontSize: "30px" }} />
         </Box>
       </Box>
+
+      {/* Box 2 */}
       <Box
         sx={{
           display: "flex",
@@ -51,12 +58,13 @@ export default function BestIcons() {
           color: "white",
           padding: "10px 15px",
           borderRadius: "20px 5px 20px 5px",
-          width: "200px",
+          width: "auto",
+          minWidth: "180px",
           justifyContent: "space-between",
           boxShadow: 3,
         }}
       >
-        <Typography sx={{ fontSize: "25px", fontWeight: "bold" }}>
+        <Typography sx={{ fontSize: "20px", fontWeight: "bold"  , fontFamily:"IranYekan"}}>
           دریافت سریع
         </Typography>
         <Box
@@ -71,7 +79,7 @@ export default function BestIcons() {
             height: "45px",
           }}
         >
-          <LocalShippingIcon sx={{ color: "black" }} />
+          <LocalShippingIcon sx={{ color: "black", fontSize: "30px" }} />
         </Box>
       </Box>
     </Stack>

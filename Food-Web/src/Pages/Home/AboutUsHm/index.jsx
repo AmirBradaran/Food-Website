1d29590c69acedf70beadd17d157a929875e278e
Amir <baradaran13085@gmail.com>
2025-03-02 13:39:11 +0330

    Update index.jsx

--- Food-Web/src/Pages/Home/AboutUsHm/index.jsx
+++ Food-Web/src/Pages/Home/AboutUsHm/index.jsx
@@ -13,21 +13,28 @@ const images = ["img1-1.jpg", "img2-1.jpg", "img3-1.jpg", "img4-1.jpg"];
 
 const FoodGallery = () => {
   return (
-    <Stack direction={"row-reverse"} alignItems={"center"} gap={0}>
-      <Box sx={{ display: "flex" }}>
-        <Grid container spacing={2} justifyContent="end">
+    <Stack
+      direction="row-reverse"
+      alignItems="center"
+      justifyContent="space-between"
+      flexWrap="wrap"
+      sx={{ width: "100%" ,  }}
+    >
+      {/* بخش گالری تصاویر */}
+      <Box sx={{ flex: 1, display: "flex", justifyContent: "center" }}>
+        <Grid container spacing={2} justifyContent="center">
           {/* ردیف اول */}
           <Grid item xs={6} md={5} display="flex" justifyContent="end">
             <Card
               sx={{
                 borderRadius: "00% 50% 00% 50%",
                 overflow: "hidden",
-                width: "300px",
+                width: "250px",
               }}
             >
               <CardMedia
                 component="img"
-                height="350px"
+                height="300px"
                 image={images[0]}
                 alt="Food 1"
               />
@@ -39,19 +46,19 @@ const FoodGallery = () => {
             md={5}
             display="flex"
             justifyContent="start"
-            alignItems={"end"}
+            alignItems="end"
           >
             <Card
               sx={{
                 borderRadius: "50% 00% 50% 00%",
                 overflow: "hidden",
-                width: "300px",
-                height: "300px",
+                width: "250px",
+                height: "250px",
               }}
             >
               <CardMedia
                 component="img"
-                height="300px"
+                height="250px"
                 image={images[1]}
                 alt="Food 2"
               />
@@ -64,13 +71,13 @@ const FoodGallery = () => {
               sx={{
                 borderRadius: "50% 00% 50% 00%",
                 overflow: "hidden",
-                width: "300px",
-                height: "300px",
+                width: "250px",
+                height: "250px",
               }}
             >
               <CardMedia
                 component="img"
-                height="300px"
+                height="250px"
                 image={images[2]}
                 alt="Food 3"
               />
@@ -81,13 +88,13 @@ const FoodGallery = () => {
               sx={{
                 borderRadius: "00% 50% 00% 50%",
                 overflow: "hidden",
-                width: "300px",
-                height: "350px",
+                width: "250px",
+                height: "300px",
               }}
             >
               <CardMedia
                 component="img"
-                height="350px"
+                height="300px"
                 image={images[3]}
                 alt="Food 4"
               />
@@ -96,19 +103,20 @@ const FoodGallery = () => {
         </Grid>
       </Box>
 
+      {/* بخش متن و دکمه */}
       <Stack
+        flex={1}
         gap={2}
-        sx={{ direction: "rtl" }}
-        justifyContent={"end"}
-        alignItems={"start"}
+        sx={{ direction: "rtl", textAlign: "right", padding: "0 20px" , alignItems:{xs:"center" , md:"start"} }}
+        justifyContent="center"
       >
-        <Typography fontFamily={"IranYekan"} variant="h3">
+        <Typography fontFamily="IranYekan" variant="h3">
           درباره رستوران
         </Typography>
-        <Typography fontFamily={"IranYekan"} variant="h4">
-          خوشمره ترین ها در اینجاست
+        <Typography fontFamily="IranYekan" variant="h4">
+          خوشمزه‌ترین‌ها در اینجاست
         </Typography>
-        <Typography fontFamily={"IranYekan"} variant="h6">
+        <Typography fontFamily="IranYekan" variant="h6">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis dolor
           voluptatem quas, tempore atque iusto!
         </Typography>
